1c5efdb2b9e88c07acb954267dc454502fc0a939
Prelang Builder <builder@prelang.com>
2014-07-21 08:58:35 +0000

    Generated model Profile with migration

---
+++ db/migrate/20140721085834_create_profiles.rb
@@ -0,0 +1,17 @@
+class CreateProfiles < ActiveRecord::Migration
+  def change
+    create_table :profiles do |t|
+      t.references :user, index: true
+      t.string :residence
+      t.string :hometown
+      t.text :education
+      t.text :skills
+      t.text :occupation
+      t.date :date_of_birth
+      t.text :about
+      t.references :user, index: true
+
+      t.timestamps
+    end
+  end
+end
