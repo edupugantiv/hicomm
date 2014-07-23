cc3c9c090dce189d25e1bb6274d1d40a9b5c4860
Kevin <kyc5cw@virginia.edu>
2014-07-23 05:53:58 -0400

    added phone number to user, name to profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  protected
+
+    def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :phone_number
+    devise_parameter_sanitizer.for(:sign_up) << :country
+    devise_parameter_sanitizer.for(:sign_up) << :preference
+    devise_parameter_sanitizer.for(:account_update) << :phone_number
+    devise_parameter_sanitizer.for(:account_update) << :country
+    devise_parameter_sanitizer.for(:sign_up) << :prefence
+  end
+
+
   private
   
   #-> Prelang (user_login:devise)
